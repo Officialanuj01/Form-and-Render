f4b354c9d3ec4e78408088ed364282dc67a4b3a2
Officialanuj01 <142708397+Officialanuj01@users.noreply.github.com>
2023-12-09 17:05:33 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import './App.css';
-import LikeImage from './components/LikeImage';
-import LikePost from './components/LikePost';
-import Likerender from './components/Likerender';
-import Postrender from './components/postrender';
-import Render from './components/render';
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './commponet/Navbar'
+import Contact from './commponet/Contact'
+import Home from './commponet/home'
+import RegistrationForm from './commponet/RegistrationForm'
+
+import './App.css'
+
 function App() {
+  
+
   return (
-    <div>
-      <h3>Some Blog</h3>
-      <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
-      </div>
-      <div>
-        <h1>Render Props</h1>
-        <Render ren ={(arr1,arr2)=>{return <Likerender count = {arr1} funct = {arr2}/>}} />
-        <Render ren ={(arr1,arr2)=>{return <Postrender count = {arr1} funct = {arr2}/>}} />
-
-
-      </div>
-    </div>
-  );
+    <>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='RegistrationForm' element={<RegistrationForm/>}/>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
